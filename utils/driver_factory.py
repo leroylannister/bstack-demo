220b7bf969bc08b9f8eaadece4aac5e81c5c190d
leroylannister <leeroycodes@gmail.com>
2025-06-25 16:26:37 -0500

    Fixing pipeline

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from config.config import Config
 
 class DriverFactory:
@@ -9,19 +9,24 @@ class DriverFactory:
         if not Config.BROWSERSTACK_USERNAME or not Config.BROWSERSTACK_ACCESS_KEY:
             raise ValueError("BrowserStack credentials not found. Please set BROWSERSTACK_USERNAME and BROWSERSTACK_ACCESS_KEY environment variables.")
         
-        # Create capabilities
-        capabilities = DesiredCapabilities.CHROME.copy()
-        capabilities.update(browser_config)
-        
-        # Add build name from Jenkins if available
         import os
         build_name = os.getenv('BUILD_NUMBER', 'Local Build')
-        capabilities['bstack:options']['buildName'] = f"BStack Demo Suite - Build {build_name}"
         
-        # Create remote driver
+        # Prepare BrowserStack options
+        bstack_options = browser_config.get('bstack:options', {})
+        bstack_options['buildName'] = f"BStack Demo Suite - Build {build_name}"
+        
+        # Remove bstack:options from browser_config to avoid duplication
+        browser_config = {k: v for k, v in browser_config.items() if k != 'bstack:options'}
+        
+        options = ChromeOptions()
+        for key, value in browser_config.items():
+            options.set_capability(key, value)
+        options.set_capability('bstack:options', bstack_options)
+        
         driver = webdriver.Remote(
             command_executor=Config.BROWSERSTACK_HUB,
-            desired_capabilities=capabilities
+            options=options
         )
         
         driver.maximize_window()
